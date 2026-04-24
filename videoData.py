aedef3dc2cc5f6402cd3c50b3e8248194eb8bb25
Meenakshi <meenakshisharma7395@gmail.com>
2026-04-24 15:46:41 +0530

    video captured and saved in mp4 formate

--- videoData.py
+++ videoData.py
@@ -6,10 +6,15 @@ cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+# WINDOWS -- *'DIVX'
+# MACOS or LINUX -- *'XVID'
+
+writer = cv2.VideoWriter('mysupervideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+
 # Print the width and height of the video frames
 while True:
     ret, frame = cap.read()
-    
+    writer.write(frame)
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Display the resulting frame in grayscale
     #cv2.imshow('frame' , gray)
@@ -21,4 +26,5 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
+writer.release()
 cv2.destroyAllWindows()
